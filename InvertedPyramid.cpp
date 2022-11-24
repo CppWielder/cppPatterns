75d051adcd58cb7e113240a073f7200ee2b6b8e2
cppweilder <cppwielder@gmail.com>
2022-11-24 08:03:34 +0530

    Laterally inverted pyramid program

---
+++ InvertedPyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+    int height;
+    std::cout<<"Enter height of pyramid :";
+    std::cin>>height;
+    for(int i = 0;i < height;i++) {
+        for(int j = 0;j < height - i - 1;j++) {
+            std::cout<<' ';
+        }
+        for(int j = 0;j < i + 1;j++) {
+            std::cout<<'*';
+        }
+        std::cout<<'\n';
+    }
+    return 0;
+}
