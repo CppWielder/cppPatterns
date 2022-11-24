b01512c064eabd968b41b6c11b94654ea51ade1c
cppweilder <cppwielder@gmail.com>
2022-11-24 08:13:24 +0530

    Reverse inverted pyramid

---
+++ ReverseInvertedPyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+    int height;
+    std::cout<<"Enter height of pyramid :";
+    std::cin>>height;
+    for(int i = 0;i < height;i++) {
+        for(int j = 0;j < i;j++) {
+            std::cout<<' ';
+        }
+        for(int j = 0;j < height - i;j++) {
+            std::cout<<'*';
+        }
+        std::cout<<'\n';
+    }
+    return 0;
+}
